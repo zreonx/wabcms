d19db9a32085ce76ea4a59ba1824e79383d9a946
Zreon <56314835+zreonx@users.noreply.github.com>
2023-02-02 11:37:49 +0800

    Create clearance form

--- admin/create_clearance.php
+++ admin/create_clearance.php
@@ -4,7 +4,46 @@
     <h1 class="panel-title">Clearance</h1>
     <div class="card">
         <div class="card-body">
-            <h1>Test</h1>
+        <h1 class="fs-6 card-title">Create Clearance</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <label class="form-label">Clearance Beneficiaries</label>
+                            <select id="disabledSelect" class="form-select">
+                                <option>Select</option>
+                                <option>All Student</option>
+                                <option>Specific</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6 d-none">
+                        <label class="form-label">Search Student </label>
+                            <input type="text" class="form-control" name="search_clearance" placeholder="Search" >
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Clearance Type</label>
+                    <select id="disabledSelect" class="form-select">
+                        <option>Select</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Academic Year</label>
+                    <input type="text" class="form-control" name="academic_year">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Semester</label>
+                    <select id="disabledSelect" class="form-select">
+                        <option>Select</option>
+                        <option>1</option>
+                        <option>2</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+       
         </div>
     </div>
     
